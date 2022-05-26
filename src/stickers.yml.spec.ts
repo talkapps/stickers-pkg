b0a5a5698eaab11a7e085b026258c64eb7fd8d45
NKDuy <kn145660@gmail.com>
2022-05-26 20:31:59 +0700

    Update stickers.yml.spec.ts

--- src/stickers.yml.spec.ts
+++ src/stickers.yml.spec.ts
@@ -11,7 +11,7 @@ import {
  * The maximum number of sticker packs a commit/PR is allowed to remove from the
  * directory.
  */
-const MAX_ALLOWED_DELETIONS = 5;
+const MAX_ALLOWED_DELETIONS = 1;
 
 
 describe('stickers.yml', () => {
